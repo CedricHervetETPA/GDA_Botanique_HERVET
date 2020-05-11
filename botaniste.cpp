fd007a1283c6bf4831e0fc5d4ec2508702a819b7
CedricHervetETPA <cedric.hervet49@gmail.com>
2020-05-11 18:16:18 +0200

    Create botaniste.cpp

---
+++ botaniste.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <limits>
+#include <string>
+#include "botaniste.hpp"
+
+main ()
+{
+    int engrais = 10;
+    int argent = 100;
+    int jours = 0;
+
+
+
+
+    return 0;
+}
+void dormir ()
+void tailler ()
+void arroser () 
+void vendre ()
+void acheterEngrais ()
+void acheterPlante ()
+void nourrirPlante ()
+void vendrePlante ()
+void examinerPlante ()
