a4f84df7b3d76119b30ac408157f86a81fe9441f
CedricHervetETPA <cedric.hervet49@gmail.com>
2020-05-11 18:15:52 +0200

    Create plantes.hpp

---
+++ plantes.hpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <limits>
+#include <string>
+
+#if !defined(fleur)
+#define fleur
+
+
+class fleur
+{
+    public:
+        fleur();
+
+
+    private:
+        std:: string nom;
+        int age;
+        int taille;
+        int sante;
+        float croissance;
+
+};
+
+class planteCarnivore
+{
+    public:
+        planteCarnivore();
+
+
+    private:
+        std:: string nom;
+        int age;
+        int taille;
+        int sante;
+        float croissance;
+
+};
